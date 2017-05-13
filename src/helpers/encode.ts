bcd713abb4f4b06327deb7886065dc7ca34cf93f
chrisguttandin <chrisguttandin@media-codings.com>
2017-05-13 16:15:41 +0200

    preencode text

--- src/helpers/encode.ts
+++ src/helpers/encode.ts
@@ -11,19 +11,10 @@ export const encode = (audioTypedArrays: TypedArray[] = [], { bitRate = 16, samp
 
     const dataView = new DataView(arrayBuffer);
 
-    dataView.setUint8(0, 'R'.charCodeAt(0));
-    dataView.setUint8(1, 'I'.charCodeAt(0));
-    dataView.setUint8(2, 'F'.charCodeAt(0));
-    dataView.setUint8(3, 'F'.charCodeAt(0));
+    dataView.setUint32(0, 1380533830); // That's the integer representation of 'RIFF'.
     dataView.setUint32(4, arrayBuffer.byteLength - 8, true);
-    dataView.setUint8(8, 'W'.charCodeAt(0));
-    dataView.setUint8(9, 'A'.charCodeAt(0));
-    dataView.setUint8(10, 'V'.charCodeAt(0));
-    dataView.setUint8(11, 'E'.charCodeAt(0));
-    dataView.setUint8(12, 'f'.charCodeAt(0));
-    dataView.setUint8(13, 'm'.charCodeAt(0));
-    dataView.setUint8(14, 't'.charCodeAt(0));
-    dataView.setUint8(15, ' '.charCodeAt(0));
+    dataView.setUint32(8, 1463899717); // That's the integer representation of 'WAVE'.
+    dataView.setUint32(12, 1718449184); // That's the integer representation of 'fmt '.
     dataView.setUint32(16, 16, true);
     dataView.setUint16(20, 1, true);
     dataView.setUint16(22, numberOfChannels, true);
@@ -31,10 +22,7 @@ export const encode = (audioTypedArrays: TypedArray[] = [], { bitRate = 16, samp
     dataView.setUint32(28, sampleRate * numberOfChannels * bytesPerSample, true);
     dataView.setUint16(32, numberOfChannels * bytesPerSample, true);
     dataView.setUint16(34, bitRate, true);
-    dataView.setUint8(36, 'd'.charCodeAt(0));
-    dataView.setUint8(37, 'a'.charCodeAt(0));
-    dataView.setUint8(38, 't'.charCodeAt(0));
-    dataView.setUint8(39, 'a'.charCodeAt(0));
+    dataView.setUint32(36, 1684108385); // That's the integer representation of 'data'.
     dataView.setUint32(40, arrayBuffer.byteLength - 44, true);
 
     audioTypedArrays
